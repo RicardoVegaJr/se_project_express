33e2e630454c1e93c995b808d233b2f801f8c071
Ricardo Vega <rvega921@gmail.com>
2025-06-20 12:50:13 -0400

    Create: Like and unlike item controllers

--- controllers/clothingItems.js
+++ controllers/clothingItems.js
@@ -1,4 +1,5 @@
 const clothingItem = require("../models/clothingItem");
+const getUsersById = require("./users");
 
 const getClothing = (req, res) => {
   clothingItem.find({})
@@ -43,5 +44,53 @@ const deleteItem = (req, res) => {
     });
 };
 
+const likeItem = (req, res) => {
+  const {itemId} = req.params;
 
-module.exports = { getClothing, postItem, deleteItem};
+  clothingItem.findByIdAndUpdate(
+    itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true }
+  )
+  .then((item) => {
+    if (!item) {
+      return res.status(404).send({ message: "Item not found" });
+    }
+    return res.status(200).send({ message: "Item liked", data: item });
+  })
+  .catch((err) => {
+    console.error(err);
+    if (err.name === 'CastError') {
+      return res.status(400).send({ message: "Invalid ID." });
+    }
+    return res.status(500).send({ message: err.message });
+  });
+};
+
+const dislikeItem = (req, res) => {
+  const {itemId} = req.params;
+
+
+  clothingItem.findByIdAndUpdate(
+    itemId,
+  { $pull: { likes: req.user._id } },
+  { new: true },
+  )
+  .then((item) => {
+    if (!item) {
+      return res.status(404).send({ message: "Item not found" });
+    }
+    return res.status(200).send({ message: "Item like deleted", data: item });
+  })
+  .catch((err) => {
+    console.error(err);
+    if (err.name === 'CastError') {
+      return res.status(400).send({ message: "Invalid ID." });
+    }
+    return res.status(500).send({ message: err.message });
+  });
+};
+
+
+
+module.exports = { getClothing, postItem, deleteItem, likeItem, dislikeItem};
--- routes/clothing.js
+++ routes/clothing.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
-const {getClothing, postItem, deleteItem} = require("../controllers/clothingItems");
+const {getClothing, postItem, deleteItem, likeItem, dislikeItem} = require("../controllers/clothingItems");
 
 router.get("/", getClothing);
 router.post("/", postItem);
 router.delete("/:itemId", deleteItem);
+router.put("/:itemId/likes", likeItem);
+router.delete("/:itemId/likes", dislikeItem);
 
 module.exports = router;
 
